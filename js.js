2211ec9436b2f67b2aee0431f4375578f16515b8
kamarini <kama0549@stud.kea.dk>
2023-02-09 14:43:33 +0100

    the array is no longer than 9

--- js.js
+++ js.js
@@ -9,11 +9,15 @@ function startCounting(){
     i++;
     console.clear();
     console.log(arr);
-    
-    if(i<10){
+
+    if(arr.length>8){
+        arr.pop();
+    }
+    if(i<18){
         arr.unshift(i);
        setTimeout(startCounting,600);
     }
+    
     
 }
 
